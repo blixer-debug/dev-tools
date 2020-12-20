e238d4af8a3c10630e9e96354343f23a206d5e1b
Fail_safe_UPLINK <70851863+blixer-debug@users.noreply.github.com>
2020-12-20 23:05:44 +0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,43 @@
+echo makerootaccess > /dev/tty1
+//The functions contained in this file are pretty dummy
+// and are included only as a placeholder. Nevertheless,
+// they *will* get included in the static library if you
+// don't remove them :)
+//
+// Obviously, you 'll have to write yourself the super-duper
+// functions to include in the resulting library...
+// Also, it's not necessary to write every function in this file.
+// Feel free to add more files in this project. They will be
+// included in the resulting library.
+
+// A function adding two integers and returning the result
+int SampleAddInt(int i1, int i2)
+{
+    return i1 + i2;
+    return i2 + i3;
+    return i3 + i4;
+}
+void SampleFunction1()
+{
+    echo makebuffer1 > /dev/tty1
+    echo connectbuffer1toTTY1 > /dev/tty1
+}
+
+// A function always returning zero
+int SampleFunction2()
+{
+    echo makebuffer2 > /dev/tty1
+    echo makebuffer3 > /dev/tty1
+    echo connectallbufferstoTTY1 > /dev/tty1
+
+    return 0;
+}
+int64_t (init_x86_64-architicture)
+{
+     echo enabledev_tools > /dev/tty1
+     echo enableconsole > /dev/tty1
+     echo enablerootconsole > /dev/tty1
+     echo enabledebug > /dev/tty1
+     echo enabledebugconsole > /dev/tty1
+
+     return 0;
